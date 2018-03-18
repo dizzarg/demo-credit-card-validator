6be82d5f7067196634507c028823120e9dd2c7e8
damir <dkadyrov@fix.ru>
2018-03-18 16:28:49 +0300

    fix build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
 	id("io.spring.dependency-management") version "1.0.4.RELEASE"
-	id('net.saliman.cobertura') version '2.3.1'
-        id('com.github.kt3k.coveralls') version '2.8.2'
+	id("com.github.kt3k.coveralls") version "2.8.2"
+	id("net.saliman.cobertura") version "2.3.1"
 }
 
-cobertura.coverageFormats = ['html', 'xml']
+cobertura.coverageFormats = ["html", "xml"]
 
 version = "0.0.1-SNAPSHOT"
 
