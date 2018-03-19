05634fdb00639b4a5cbb52230e4c5ab911b7d3ab
dkadyrov <dkadyrov@fix.ru>
2018-03-19 14:53:35 +0300

    fix build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-
 plugins {
 	val kotlinVersion = "1.2.21"
 	id("org.springframework.boot") version "2.0.0.RELEASE"
@@ -8,21 +7,27 @@ plugins {
 	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
 	id("io.spring.dependency-management") version "1.0.4.RELEASE"
-	id("com.github.kt3k.coveralls") version "2.8.2"
-	id("info.solidsoft.pitest") version "1.1.11"
+	jacoco
+	id("com.github.kt3k.coveralls") version "2.8.1"
 }
 
 version = "0.0.1-SNAPSHOT"
 
-pitest {
-	timestampedReports = false
-	outputFormats = ['XML']
+jacoco {
+
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		jvmTarget = "1.8"
 		freeCompilerArgs = listOf("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<JacocoReport> {
+	reports {
+		html.isEnabled = true
+		xml.isEnabled = true
 	}
 }
 
